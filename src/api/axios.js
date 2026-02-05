12dac4846b4874dd22b2f8bbdc8d21513db2f746
sellchatai <sellchatsai@gmail.com>
2026-02-05 11:06:02 +0530

    Fix admin root routing to login

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://vidoprompt-backend.vercel.app/api",
 });
 
 export default api;
